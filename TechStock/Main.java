20a121f4dc2108408043f7229735d31ed2a360a1
saybq <dolorclintadrelle@gmail.com>
2025-06-09 16:52:33 +0800

    Update Main.java

--- TechStock/Main.java
+++ TechStock/Main.java
@@ -2,6 +2,8 @@ package IMS;
 
 public class Main {
 	public static void main(String[] args) {	
+		System.setProperty("sun.java2d.uiScale", "0.9");
+		
 		javax.swing.SwingUtilities.invokeLater(() -> {
 			new LaunchFrame();
         });	
